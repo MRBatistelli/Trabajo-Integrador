ebd2113eeb2156d03fb981030efdc686402d7d4b
MRBatistelli <maurobati1987@gmail.com>
2024-05-02 16:34:56 -0300

    Agregamos mas tarjetas e imagenes

--- JS/responsiveMenu.js
+++ JS/responsiveMenu.js
@@ -1,3 +1,64 @@
+//Selecionar etiquetas
+const header = document.querySelector('#header');
+const footer = document.querySelector('#footer');
+
+
+header.innerHTML =`<section class="nav__logo">
+<img class="nav__img" src="./assets/img/logo.png" alt="logo">
+<a href="index.html" class="nav__index">
+    <h1 class="nav__title">LOS INDOMABLES</h1>
+    <p>Fuerza y Pasión sobre Ruedas</p>
+</a>
+</section>
+
+<div class="responsive__menu-abrir" id="abrir"><i class="fa-solid fa-bars"></i></div>
+
+
+<nav class="nav__conteiner" id="nav">
+
+<div class="responsive__menu-cerrar" id="cerrar"><i class="fa-solid fa-xmark"></i></div>
+
+<ul class="list">
+    <li class="list__li"><a class="list__item list--register" href="./register.html">Registrarse</a></li>
+    <li class="list__li"><a class="list__item list--login" href="./login.html">Ingresa <i
+                class="fa-solid fa-right-to-bracket"></i></a></li>
+    <li class="list__li"><a class="list__item list--cart" href="./buy.html"><i
+                class="fa-solid fa-cart-shopping"></i></a></li>
+</ul>
+
+</nav>`,
+
+footer.innerHTML=`<div class="footer__nav">
+<div class="img__conteiner"><img class="footer__img" src="./assets/img/logo.png" alt="logo"></div>
+<section>
+    <nav>
+        <ul class="nav__list">
+            <li class="nav__item"><a href="./about.html">Acerca de Nosotros</a></li>
+            <li class="nav__item"><a href="./contact.html">Contacto</a></li>
+            <li class="nav__item"><a href="./paymentMethods.html">Medios de Pago</a></li>
+            <li class="nav__item"><a href="./warranty.html">Garantia</a></li>
+        </ul>
+    </nav>
+</section>
+<section>
+    <nav>
+        <ul class="nav__list">
+            <li class="nav__item"><a href="https://twitter.com/?lang=es" target="_blank"><i
+                        class="fa-brands fa-x-twitter"></i></a></li>
+            <li class="nav__item"><a href="https://www.instagram.com/" target="_blank"><i
+                        class="fa-brands fa-instagram"></i></a>
+            </li>
+            <li class="nav__item"><a href="https://www.facebook.com/" target="_blank"><i
+                        class="fa-brands fa-facebook"></i></a>
+            </li>
+            <li class="nav__item"><a href="https://www.linkedin.com/" target="_blank"><i
+                        class="fa-brands fa-linkedin"></i></a>
+            </li>
+        </ul>
+    </nav>
+</section>
+</div>`;
+
 //selecionar etiquetas
 const abrirMenu = document.querySelector("#abrir");
 const cerrarMenu = document.querySelector("#cerrar");
